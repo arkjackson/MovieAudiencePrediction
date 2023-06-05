4d40deb2c9107e94019968fb1aaec430c0f55e67
arkjackson <jackson0511@naver.com>
2023-06-06 00:55:13 +0900

    Change data file from '.csv ' to '.txt' and Count total number of Noun, Verb, Adjective from a movie

--- Crawling/morpheme_analysis.py
+++ Crawling/morpheme_analysis.py
@@ -1,26 +1,27 @@
 # 형태소 분석 프로그램
+# 영화 별 명사, 동사, 형용사 수 저장
 
-import pandas as pd
 from konlpy.tag import Okt
 
-# csv파일 읽어오기
-csv = pd.read_csv(r'C:\Users\82104\Desktop\movie_review_2021.csv',\
-                  names=['movie_title', 'released_date', 'comment', 'writing_date', 'star_score', 'upvote', 'downvote', 'save_date'],\
-                  encoding='CP949')
+# .csv 파일 대신 .txt 파일 읽기
+filename = 'C:/Users/82104/Desktop/movie_name.txt'
+f = open(filename, 'r', encoding='utf8')
+sentences = f.readlines()
 # Open Korean Text: 오픈 소스 한국어 분석기. 과거 트위터 형태소 분석기.
 okt = Okt()
-# 명사, 동사, 형용사 별로 개수 분류
-for i in range(1, len(csv['comment'])):
-    if str(csv['comment'][i]) == 'nan':
-        continue
-    dic = {'Noun': 0, 'Verb': 0, 'Adjective': 0}
-    word_class = okt.pos(csv['comment'][i])
-    for j in range(len(word_class)):
-        if word_class[j][1] == 'Noun':
-            dic['Noun'] += 1
-        elif word_class[j][1] == 'Verb':
-            dic['Verb'] += 1
-        elif word_class[j][1] == 'Adjective':
-            dic['Adjective'] += 1
-    print(i)
-    print(csv['movie_title'][i], dic)
+# 형태소 저장 변수
+dic = {'Noun': 0, 'Verb': 0, 'Adjective': 0}
+for i in range(len(sentences)):
+    # 공백 리뷰가 아닌 경우
+    if sentences[i] != "\n":
+        print(sentences[i])
+        word_class = okt.pos(sentences[i])  # 품사 부착
+        for j in range(len(word_class)):
+            if word_class[j][1] == 'Noun':  # 명사
+                dic['Noun'] += 1
+            elif word_class[j][1] == 'Verb':    # 동사
+                dic['Verb'] += 1
+            elif word_class[j][1] == 'Adjective':   # 형용사
+                dic['Adjective'] += 1
+# 결과 출력
+print(dic)
