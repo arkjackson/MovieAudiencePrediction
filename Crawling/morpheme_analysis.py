f3e430687cbf08cbcff4ce201324d3c942595efd
arkjackson <jackson0511@naver.com>
2023-06-06 00:58:22 +0900

    Change the Variable Name which have total number of each morpheme

--- Crawling/morpheme_analysis.py
+++ Crawling/morpheme_analysis.py
@@ -10,7 +10,7 @@ sentences = f.readlines()
 # Open Korean Text: 오픈 소스 한국어 분석기. 과거 트위터 형태소 분석기.
 okt = Okt()
 # 형태소 저장 변수
-dic = {'Noun': 0, 'Verb': 0, 'Adjective': 0}
+mor_dic = {'Noun': 0, 'Verb': 0, 'Adjective': 0}
 for i in range(len(sentences)):
     # 공백 리뷰가 아닌 경우
     if sentences[i] != "\n":
@@ -18,10 +18,10 @@ for i in range(len(sentences)):
         word_class = okt.pos(sentences[i])  # 품사 부착
         for j in range(len(word_class)):
             if word_class[j][1] == 'Noun':  # 명사
-                dic['Noun'] += 1
+                mor_dic['Noun'] += 1
             elif word_class[j][1] == 'Verb':    # 동사
-                dic['Verb'] += 1
+                mor_dic['Verb'] += 1
             elif word_class[j][1] == 'Adjective':   # 형용사
-                dic['Adjective'] += 1
+                mor_dic['Adjective'] += 1
 # 결과 출력
-print(dic)
+print(mor_dic)
