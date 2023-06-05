e7d3bb3e72eaf0fc19e1b51dca174ebbf37f346d
arkjackson <jackson0511@naver.com>
2023-06-06 01:21:39 +0900

    Change the variable 'mor_dic' to 'mor_list' list type And Add function of save results to '.csv'

--- Crawling/morpheme_analysis.py
+++ Crawling/morpheme_analysis.py
@@ -2,26 +2,41 @@
 # 영화 별 명사, 동사, 형용사 수 저장
 
 from konlpy.tag import Okt
+import pandas as pd
+
+# 결과값 .csv파일로 저장
+def save_results_to_csv(lst):
+    final=[]
+    final.append(lst)
+    # 데이터프레임 생성
+    df = pd.DataFrame(final, columns=['Noun', 'Verb', 'Adjective'])
+    df.to_csv("C:/Users/82104/Desktop/morphe.csv", index=False)
+# 형태소 분석 이후 명사, 동사, 형용사 개수 카운트
+def count_morpheme(s):
+    word_class = okt.pos(s)  # 품사 부착
+    for j in range(len(word_class)):
+        if word_class[j][1] == 'Noun':  # 명사
+            mor_list[0] += 1
+        elif word_class[j][1] == 'Verb':  # 동사
+            mor_list[1] += 1
+        elif word_class[j][1] == 'Adjective':  # 형용사
+            mor_list[2] += 1
 
 # .csv 파일 대신 .txt 파일 읽기
-filename = 'C:/Users/82104/Desktop/movie_name.txt'
+filename = 'C:/Users/82104/Desktop/영화댓글수집/영화/대호.txt'
 f = open(filename, 'r', encoding='utf8')
 sentences = f.readlines()
 # Open Korean Text: 오픈 소스 한국어 분석기. 과거 트위터 형태소 분석기.
 okt = Okt()
 # 형태소 저장 변수
-mor_dic = {'Noun': 0, 'Verb': 0, 'Adjective': 0}
+mor_list = [0, 0, 0]
 for i in range(len(sentences)):
-    # 공백 리뷰가 아닌 경우
+    # 공백 리뷰가 아닌 경우만 형태소 분석 실행
     if sentences[i] != "\n":
         print(sentences[i])
-        word_class = okt.pos(sentences[i])  # 품사 부착
-        for j in range(len(word_class)):
-            if word_class[j][1] == 'Noun':  # 명사
-                mor_dic['Noun'] += 1
-            elif word_class[j][1] == 'Verb':    # 동사
-                mor_dic['Verb'] += 1
-            elif word_class[j][1] == 'Adjective':   # 형용사
-                mor_dic['Adjective'] += 1
+        # 형태소 개수 저장
+        count_morpheme(sentences[i])
+# 결과 .csv 파일로 저장
+save_results_to_csv(mor_list)
 # 결과 출력
-print(mor_dic)
+print(mor_list)
